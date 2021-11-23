85eeb2acc80aa45ce1b191feb2162ef6104b744d
GoatGirl98 <47886494+GoatGirl98@users.noreply.github.com>
2021-11-23 17:20:34 +0800

    Update Hack for BUAAOJ40 (to be continued)

--- 2014级-软件学院-算法分析与设计/E1-算法练习赛/BUAAOJ40 计算几何 分治 最近点对.cpp
+++ 2014级-软件学院-算法分析与设计/E1-算法练习赛/BUAAOJ40 计算几何 分治 最近点对.cpp
@@ -1,89 +1,86 @@
-#include<iostream>
-#include<climits>
-#include<cmath>
-#include<cstring>
-#include<algorithm>
-#define MAXN 1000010
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <cstdio>
+#include <cmath>
 
 using namespace std;
 
-//稍微使用一些计算几何的知识，合并计算点对的时候，每个点找对面6个点就足够了
+const int N = 200005;
+const double INF = 1e15;
+const double eps = 1e-8;
 
-const int INF = 2 << 20;
 int n;
-int temp[MAXN];
+double mind;
+struct point
+{
+    double x, y;
+    bool type;
+    bool operator<(const point &t) const { return x < t.x; }
+} points[N], tmp[N];
 
-struct Node {
-	double x;
-	double y;
-	double prop;
-}S[MAXN];
-
-bool cmp(const Node& a, const Node& b) {
-	if (a.x == b.x)return a.y < b.y;
-	else return a.x < b.x;
+double get_dist(point a, point b)
+{
+    if (a.type == b.type)
+        return mind; // optimize when all the points are overlapped
+    double dx = a.x - b.x, dy = a.y - b.y;
+    return sqrt(dx * dx + dy * dy);
 }
 
-bool cmpIndex(const int& a, const int& b) {
-	return S[a].y < S[b].y;
+double dfs(int l, int r)
+{
+    if (l == r)
+        return INF;
+    int mid = (l + r) >> 1;
+    double mid_x = points[mid].x;
+    double ans = min(dfs(l, mid), dfs(mid + 1, r));
+
+    int i = l, j = mid + 1, cnt = 0;
+    while (i <= mid && j <= r)
+        if (points[i].y < points[j].y)
+            tmp[cnt++] = points[i++];
+        else
+            tmp[cnt++] = points[j++];
+    while (i <= mid)
+        tmp[cnt++] = points[i++];
+    while (j <= r)
+        tmp[cnt++] = points[j++];
+    for (i = l; i <= r; i++)
+        points[i] = tmp[i - l];
+
+    cnt = 0;
+    for (i = l; i <= r; i++)
+        if (points[i].x >= mid_x - ans && points[i].x <= mid_x + ans)
+            tmp[cnt++] = points[i];
+    for (i = 0; i < cnt; i++)
+        for (j = i - 1; ~j && tmp[i].y - tmp[j].y + eps <= ans; j--)
+            ans = min(ans, get_dist(tmp[i], tmp[j]));
+    mind = min(mind, ans);
+    return ans;
 }
 
-double calculate(int i, int j) {
-	if (S[i].prop == S[j].prop)return LLONG_MAX;
-	double x = (S[i].x - S[j].x) * (S[i].x - S[j].x);
-	double y = (S[i].y - S[j].y) * (S[i].y - S[j].y);
-	return sqrt(x + y);
-}
-//还是左闭右闭的形式我更能接受，等到用STL等的时候再记住是左闭右开吧
-double merge_judge(int lo, int hi) {
-	//printf("get here! %d %d \n", lo, hi);
-	double distance = INF;
-	if (lo == hi)return distance;
-	if (lo + 1 == hi)return calculate(lo, hi);
-
-	int mi = (lo + hi) >> 1;
-	double d1 = merge_judge(lo, mi);
-	double d2 = merge_judge(mi + 1, hi);
-	distance = min(d1, d2);
-
-	int considerCounter = 0;
-
-	for (int i = lo; i <= hi; ++i) {
-		if (fabs(S[i].x - S[mi].x) <= distance)
-			temp[considerCounter++] = i;
-	}
-	sort(temp, temp + considerCounter, cmpIndex);
-	for (int i = 0; i < considerCounter; ++i) {
-		for (int j = i + 1; (j < considerCounter) && (S[temp[j]].y - S[temp[i]].y < distance); ++j) {
-			double d3 = calculate(temp[i], temp[j]);//此处还有另外一个条件做限制，首先他们的x已经约束了，y也通过这个约束，保证这一步一定是O(n)的
-			if (distance > d3)distance = d3;
-		}
-	}
-	return distance;
-}
-int k;
-int main() {
-
-	ios::sync_with_stdio(false);
-
-	while (scanf("%d", &k) != EOF) {
-		while (k--) {
-			scanf("%d", &n);
-			for (int i = 0; i < n; ++i) {
-				scanf("%lf%lf", &S[i].x, &S[i].y);
-				S[i].prop = 1;
-			}
-			for (int i = 0; i < n; ++i) {
-				scanf("%lf%lf", &S[n + i].x, &S[n + i].y);
-				S[n + i].prop = 2;
-			}
-			sort(S, S + 2 * n, cmp);
-			double result = merge_judge(0, 2 * n - 1);
-			printf("%.3lf\n", result);
-			memset(temp, 0, sizeof(temp));
-		}
-	}
-	return 0;
-
-
+int main()
+{
+    int T;
+    while (scanf("%d", &T) != EOF)
+    {
+        while (T--)
+        {
+            scanf("%d", &n);
+            for (int i = 0; i < n; i++)
+            {
+                scanf("%lf %lf", &points[i].x, &points[i].y);
+                points[i].type = false;
+            }
+            for (int i = n; i < n << 1; i++)
+            {
+                scanf("%lf %lf", &points[i].x, &points[i].y);
+                points[i].type = true;
+            }
+            mind = get_dist(points[0], points[(n << 1) - 1]);
+            sort(points, points + (n << 1));
+            printf("%.3f\n", dfs(0, (n << 1) - 1));
+        }
+    }
+    return 0;
 }
